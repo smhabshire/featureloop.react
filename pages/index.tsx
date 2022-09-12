51cd020db9f9a42d10f2355ba220f10432452a18
smhabshire <smhabshire@gmail.com>
2022-09-11 21:38:19 -0500

    Style nav bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,77 @@
+import { useState } from "react";
+import { FaInbox, FaChartLine, FaSatelliteDish, FaAlignJustify, FaMap, FaQuestion, FaUsers, FaCog } from "react-icons/fa";
 
-export default function Home() {
+const menu = [
+  {
+    header: "Quick Access",
+    menuItems: [
+      {
+        title: "Inbox",
+        icon: <FaInbox />,
+      },
+      {
+        title: "Statistics",
+        icon: <FaChartLine />,
+      },
+      {
+        title: "Live Board",
+        icon: <FaSatelliteDish />,
+      },
+    ],
+  },
+  {
+    header: "Modules",
+    menuItems: [
+      {
+        title: "All Posts",
+        icon: <FaAlignJustify />,
+      },
+      {
+        title: "Roadmap",
+        icon: <FaMap />,
+      },
+      {
+        title: "Changelog",
+        icon: <FaQuestion />,
+      },
+    ],
+  },
+  {
+    header: "Organization",
+    menuItems: [
+      {
+        title: "Team Members",
+        icon: <FaUsers />,
+      },
+      {
+        title: "Settings",
+        icon: <FaCog />,
+      },
+    ],
+  },
+];
+
+const MenuGroup = ({ header, menuItems }) => (
+  <>
+    <h2 className="text-slate-400 font-bold text-sm uppercase pb-2">{header}</h2>
+    <ul className="pb-7">
+      {menuItems.map(({ title, icon }) => (
+          <div className="flex items-center px-4 py-1.5 text-gray-700 rounded-md hover:cursor-pointer hover:bg-gray-100">
+            {icon}<li className="mx-4 font-bold text-xs">{title}</li>
+          </div>
+        ))}
+    </ul>
+  </>
+);
+
+export default function Navbar() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <nav className="navigation">
+      <div className="px-4 py-1.5">
+        {menu.map(({ header, menuItems }) => (
+          <MenuGroup header={header} menuItems={menuItems} />
+        ))}
+      </div>
+    </nav>
+  );
 }

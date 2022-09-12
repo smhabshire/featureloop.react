4690620bc08c2e50bb9e92e28814ee3c299b913c
Shawn Abshire <abshire.shawn@gmail.com>
2022-09-12 18:17:06 -0500

    Added some queries.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
+import {
+  QueryClient,
+  QueryClientProvider
+} from "@tanstack/react-query";
 import 'tailwindcss/tailwind.css';
+
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import '../styles/global.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient();
+
+function Featureloop({ Component, pageProps }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default Featureloop;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { FaInbox, FaChartLine, FaSatelliteDish, FaAlignJustify, FaMap, FaQuestion, FaUsers, FaCog } from "react-icons/fa";
+import { FaAlignJustify, FaChartLine, FaCog, FaInbox, FaMap, FaQuestion, FaSatelliteDish, FaUsers } from "react-icons/fa";
+import { useChangelogCategoriesQuery } from '../generated/graphql';
 
 const menu = [
   {
@@ -55,21 +55,25 @@ const MenuGroup = ({ header, menuItems }) => (
   <>
     <h2 className="text-slate-400 font-bold text-sm uppercase pb-2">{header}</h2>
     <ul className="pb-7">
-      {menuItems.map(({ title, icon }) => (
-          <div className="flex items-center px-4 py-1.5 text-gray-700 rounded-md hover:cursor-pointer hover:bg-gray-100">
-            {icon}<li className="mx-4 font-bold text-xs">{title}</li>
-          </div>
-        ))}
+      {menuItems.map(({ title, icon }, index) => (
+        <div key={index} className="flex items-center px-4 py-1.5 text-gray-700 rounded-md hover:cursor-pointer hover:bg-gray-100">
+          {icon}<li className="mx-4 font-bold text-xs">{title}</li>
+        </div>
+      ))}
     </ul>
   </>
 );
 
 export default function Navbar() {
+  const { data } = useChangelogCategoriesQuery();
+
+  console.log('data', data);
+
   return (
     <nav className="navigation">
       <div className="px-4 py-1.5">
-        {menu.map(({ header, menuItems }) => (
-          <MenuGroup header={header} menuItems={menuItems} />
+        {menu.map(({ header, menuItems }, index) => (
+          <MenuGroup key={index} header={header} menuItems={menuItems} />
         ))}
       </div>
     </nav>

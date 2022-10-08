e6010b49c7ef8f5605b5fea948086c1126b5f346
ShawnAbshire <abshire.shawn@gmail.com>
2022-10-08 00:40:48 -0500

    Added session callback to include user id.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,6 +12,14 @@ export const authOptions = {
   jwt: {
     secret: process.env.NEXTAUTH_SECRET,
   },
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
   debug: true
 }
